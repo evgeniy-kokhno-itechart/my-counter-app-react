1b785b3582acb9248fd99bf4b6136394168f26c1
Yauheni Kokhna <yauheni.kokhna@itechart-group.com>
2019-03-15 17:31:49 +0300

    Lesson 26 Lifting the state up to App component

---
+++ src/components/counters.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import Counter from "./counter";
+
+class Counters extends Component {
+  render() {
+    return (
+      <div>
+        <button
+          onClick={this.props.onReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
+        {this.props.counters.map(c => (
+          <Counter
+            key={c.id}
+            onDelete={this.props.onDelete}
+            onIncrement={this.props.onIncrement}
+            counter={c}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Counters;
